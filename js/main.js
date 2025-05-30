9f30da9946beb112866d98b600650b456d781faf
ilkin <ilkinzekeriyyeyev7@gmail.com>
2025-05-30 11:36:51 +0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+
+document.querySelector('.show-btn').addEventListener('click', function() {
+  window.location.href = 'https://www.artbreeder.com/';
+});
+document.getElementById('openEditor').addEventListener('click', function() {
+  window.location.href = 'create.html';
+});
+document.getElementById('startTrial').addEventListener('click', function() {
+      window.location.href = 'premium.html';
+    });
